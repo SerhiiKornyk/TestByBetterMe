4e65c75c1312f12981c96ca5b7156ed63a0c70ae
Arctic <arctic@inernetbs.net>
2025-03-15 20:14:15 +0200

    finished

---
+++ feature-movies/src/main/java/app/bettermetesttask/movies/injection/MoviesFragmentBuildersModule.kt
@@ -0,0 +1,14 @@
+package app.bettermetesttask.movies.injection
+
+import app.bettermetesttask.featurecommon.injection.scopes.FragmentScope
+import app.bettermetesttask.movies.sections.compose.MoviesComposeFragment
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+@Module
+abstract class MoviesFragmentBuildersModule {
+
+    @FragmentScope
+    @ContributesAndroidInjector(modules = [MoviesScreenModule::class])
+    abstract fun createMoviesComposeFragmentInjector(): MoviesComposeFragment
+}
